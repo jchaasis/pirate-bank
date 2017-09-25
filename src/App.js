cafd3084e233794300a3b955efd59b99d0fe2088
jchaasis <jchaasis@gmail.com>
2017-09-25 15:16:01 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//import actions from store
+import { buyAction, sellAction, exRateAction, reducer } from './store';
+
+//import redux
+import { connect } from 'react-redux';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    setInterval(this.props.update, 2000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+
+        <div>
+        <ul>
+          <li><p>doubloons: {this.props.doubloons}</p></li>
+          <li><p>arrrcoins: {this.props.arrrcoins}</p></li>
+          <li><p>exchange rate: {this.props.exchangeRate}</p></li>
+        </ul>
+
+        </div>
+        <div className="buttons">
+
+          <button onClick={() => this.props.buy()}>Buy</button>
+
+          <button onClick={() => this.props.sell()}>Sell</button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+  //bring in the state info from the store
+  function mapStateToProps(state) {
+    return{
+      doubloons: state.doubloons,
+      arrrcoins: state.arrrcoins,
+      exchangeRate: state.exchangeRate
+    }
+  }
+
+  //connect functions from the app to actions
+  //in the store so we can modify state
+  function mapActionsToProps(dispatch) {
+    return {
+      buy: function(){
+        dispatch(buyAction);
+      },
+      sell: function(){
+        dispatch(sellAction);
+      },
+      update: function(){
+        dispatch(exRateAction);
+      }
+    }
+  }
+
+
+export default connect(mapStateToProps, mapActionsToProps)(App);
---
+++ src/store.js
@@ -0,0 +1,75 @@
+
+//doubloons, arrrcoins, exchange rate,
+
+//import cool things from redux to help us
+//extablish the store
+import { createStore } from 'redux';
+
+//create actions that will be performed to
+//adjust the state
+
+//action for when we buy arrrcoins
+export const buyAction = {
+  type: 'BUY',
+
+}
+
+//when we want to sell arrrcoins
+export const sellAction = {
+  type: 'SELL',
+
+}
+//when the exchange rate updates
+export const exRateAction = {
+  type: 'UPDATE',
+}
+
+//set the initial exchange rate
+let exchangeRate = 1;
+
+//create a random rate
+export function randomizeRate(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return exchangeRate = Math.floor(Math.random() * (max - min + 1)) + min; //source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+
+
+export function reducer(state, action) {
+  //if the action performed is a buy
+  if (action.type === 'BUY' && state.arrrcoins >= 1){
+      return { doubloons: state.doubloons + exchangeRate,
+              arrrcoins: state.arrrcoins - 1,
+              exchangeRate: exchangeRate,
+      }
+  }
+
+  //if the action performed is a sell
+  if (action.type === 'SELL' && state.doubloons >= exchangeRate){
+      return { doubloons: state.doubloons - exchangeRate,
+              arrrcoins: state.arrrcoins + 1,
+              exchangeRate: exchangeRate,
+      }
+  }
+
+  //when the update action is performed
+  if (action.type === 'UPDATE'){
+      randomizeRate(0,10);
+      return {
+              doubloons: state.doubloons,
+              arrrcoins: state.arrrcoins,
+              exchangeRate: exchangeRate
+      }
+  }
+
+  return state;
+}
+
+//set the initial values for state
+export const store = createStore(reducer, {
+  doubloons: 10,
+  arrrcoins: 10,
+  exchangeRate: exchangeRate,
+},
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
